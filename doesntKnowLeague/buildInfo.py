0e3c4ca808dd00a5a653ad50901b2c34d9ed783b
Claude Zheng <yzhe288@emory.edu>
2023-03-13 10:28:26 -0400

    an unfinished outlines of parts that may appear in doesntKnowLeague sections

---
+++ doesntKnowLeague/buildInfo.py
@@ -0,0 +1,7 @@
+
+def buildInfo():
+    buildInfo = {
+        'state': 'buildInfo',
+        '`That\'s fine. Do you play League of Legends?.`': 'end'
+    }
+    return buildInfo
---
+++ doesntKnowLeague/laneInfo.py
@@ -0,0 +1,6 @@
+def laneInfo():
+    laneInfo = {
+        'state': 'laneInfo',
+        '`That\'s fine. Do you play League of Legends?.`': 'end'
+    }
+    return laneInfo
