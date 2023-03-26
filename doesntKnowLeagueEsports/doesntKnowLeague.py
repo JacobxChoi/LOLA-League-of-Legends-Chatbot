05b3c138e806614049905b1aea14d21480562031
rsriv26 <rishie.srivastava@emory.edu>
2023-03-26 14:01:58 -0400

    Cleaned up system responses, added structure for item and base info.

--- doesntKnowLeagueEsports/doesntKnowLeague.py
+++ doesntKnowLeagueEsports/doesntKnowLeague.py
@@ -9,30 +9,37 @@ laneInfo = laneInfo()
 def doesntKnowLeague():
     doesntKnowLeague = {
         'state': 'doesntKnowLeagueEsports',
-        '`That\'s fine. Do you play League of Legends?.`': {
+        '`That\'s fine. Have you ever played League of Legends?.`': {
             '[{yes, yeah}]': {
                 'state': 'familiarity',
-                '`Who\'s your favorite champion?`': {
+                '`Do you have a favorite champion?`': {
                     '#UserChamp': {  # ontology for favorite champion. Does not unfortunately handle cases where user has no favorite champion
                         '$FAV_CHAMP `is fun to play!` $PLAYER_RECOMMEND`plays them as well, and they\'re really fun to watch. I suggest watching them`': 'end'
+                    },
+                    '[{no, not really, not}]': {
+                        '`All good. My favorite champion is Irelia. I like playing aggressively and getting kills, so I find playing Irelia pretty fun.`': 'end'
                     },
                     'error': 'end'
                 }
             },
             '[{no, not, dont}]': {
                 'state': 'explainLeague',
-                '`That\'s fine. League is just an online 5 v 5 game, where players play champions that each have unique abilites, and the objective of the game is to kill the enemy player\'s nexus. Do you have some specific questions?`': {
-                    '[yes]': {
+                '`No problem. League is an online 5 vs 5 game, where players play "champions" that each have unique '
+                'abilites. The objective of the game is to get to the opposing team\'s base and destroy their nexus. '
+                'Do you have questions about any of that?`': {
+                    '[no, nope, not really]': {
                         '`Nice! What got you interested in League?`': {
-                            '[any response]': {
+                            'error': {
                                 '`That\'s awesome! If you want to learn more, you should watch this player. He\'s pretty famous in the community, and I think it would be a good way to learn about the community.`': 'end'
-                            },
-                            'error': 'end'
+                            }
                         }
                     },
-                    '[no]': {
+                    '[yes, yeah, kind of, kinda]': {
                         '`What are you confused about?`': {
-                            '[lane]': 'laneRole'
+                            '[{lane, role}]': 'laneRole',
+                            '[{item}]': 'items',
+                            '[{champion, champ}]': 'championInfo',
+                            '[{base, nexus}]': 'base'
                         }
                     },
                     'error': 'end'
@@ -44,8 +51,8 @@ def doesntKnowLeague():
 
     laneRole = {
         'state': 'laneRole',
-        '`Yeah, there are 5 lanes in a typical League game, and players for each lane have very different Roles`': {
-            '[{5, five}, lanes]': {
+        '`So there are 3 lanes in League, and the 5 players for each team each fill 1 role and are distributed across those 3 lanes`': {
+            '[{3, three}, lanes]': {
                 '`You maybe heard some of them, and they are Top, Jungle, Mid, ADC and Support. People often choose '
                 'their favorite lanes according to their personality and unique playing habits. They often jump '
                 ' between the roles for better team cooperation. `'
@@ -61,8 +68,8 @@ def doesntKnowLeague():
                                 '`Great! Sounds like you are an 1v1 combat fans who want to overwhelm you foes with '
                                 'superb skills. There are actually several archetypes for a typical top lane '
                                 'champion, like tanks and duelists. Do you want to know more about them? `': {
-                                    '[tank]': 'end',  # TODO add infos on tank
-                                    '[duelist]': 'end',  # TODO add infos on duelists
+                                    '[tank]': 'end',  # TODO: add infos on tank
+                                    '[duelist]': 'end',  # TODO: add infos on duelists
                                     'error' : 'end'
                                 }
 
@@ -74,9 +81,23 @@ def doesntKnowLeague():
                     'error': 'end'
                 }
             },
-            'error': 'end'# TODO: modify the diagram flow for more flexible transitions
+            'error': 'end'  # TODO: modify the diagram flow for more flexible transitions
 
         }
 
     }
-    return doesntKnowLeague, laneRole
+
+    items = {
+        'state': 'items',
+        '`Items are intimidating at first glance, but they\'re actually pretty simple.`': 'end'  # TODO: add info about items
+    }
+
+    base = {
+        'state': 'base',
+        '`Each team has their base at opposite corners of the map. The base is where players respawn after getting '
+        'killed, as well as where they can heal their HP and buy items. The nexus is in the center of the base, '
+        'protected by 2 turrets which do massive damage to champions in their range. Once one team destroys their '
+        'opponents nexus, they win the game.`': 'end'
+    }
+
+    return doesntKnowLeague, laneRole, items, base
