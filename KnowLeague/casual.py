ab89fd176a841ad41d9982294003e752e0da8744
choijacob1015 <choijacob1015@gmail.com>
2023-03-29 14:08:10 -0400

    added questions about  other tournaments that user watched this year

--- KnowLeague/casual.py
+++ KnowLeague/casual.py
@@ -1,7 +1,7 @@
 def casual():
     casual = {
         'state': 'casual', #at this point, they don't have a favorite professional player. They have a favorite region, but are not watching any games currently
-        '`I\'ll share some of my favorite teams. Jojopyun is my favorite player, but my favorite team is Edward Gaming. They\'re fun to watch`': { #user will ask questions
+        '#GATE`I\'ll share some of my favorite teams. Jojopyun is my favorite player, but my favorite team is Edward Gaming. They\'re fun to watch`': { #user will ask questions
             '{[like, too]}':{#I like them too!
                 '`Awesome! What do you like about Edward Gaming?`':'end'
             },
@@ -18,6 +18,25 @@ def casual():
                 '`What do you like about this team?`':'end'
             },
             'error':'end'
-        }
+        },
+        '#GATE `Are there any games or tournaments that you watched this year?`':{ #user didn't watch Lola suggested games, but perhaps they still watched a game this year
+            '[{yes, yeah, watched, watch}]':{ #yeah! this year, I watched a game between team1 and team2
+                '`That\'s cool! Who was the highlight of the game?`' :{
+
+                },
+                '`Nice! Who won the game? Was it close?`':{
+                    '[{yes, yeah, close}]':{ #yeah, it was a really close game!
+                        '`awesome!`':'end'
+                    },
+                    '[{no, not}]':{ #no, the game wasnt really close
+                        '`Dang. I\'m not a fan of one-sided games either.`':'end'
+                    }
+                }
+            },
+            '[{no, did not, didnt}]':{
+                '`no worries!`':'end'
+            },
+            'error':'end'
+        } #TODO: add another conversation so that gate is not stuck
     }
     return casual
