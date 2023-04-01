4f2ca42c20b5f678d67cf34e2e3ac807f9784a10
Claude Zheng <yzhe288@emory.edu>
2023-04-01 16:46:00 -0400

    Modify macros and resources/open_api.text for using gpt3.5

--- macros.py
+++ macros.py
@@ -1,10 +1,17 @@
 import json
-import spacy
-
-from emora_stdm import Macro, Ngrams
-from typing import Dict, Any, List
-import re
 import random
+import re
+from typing import Dict, Any, List, Callable, Pattern
+from json import JSONDecodeError
+
+import spacy
+from emora_stdm import Macro, Ngrams
+import openai
+import regexutils
+
+OPENAI_API_KEY_PATH = '../resources/openai_api.txt'
+CHATGPT_MODEL = 'gpt-3.5-turbo'
+
 
 class MacroGetName(Macro):
     def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
@@ -58,9 +65,11 @@ class MacroGetNewName(Macro):
         vn = vars['FIRSTNAME']
         return not vars[vn]
 
+
 class MacroPushName(Macro):
     def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
         return vars['FIRSTNAME']
+
 
 class MacroEsportsOrLeague(Macro):
     def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
@@ -71,6 +80,7 @@ class MacroEsportsOrLeague(Macro):
             if m.group(1) or m.group(2) is not None:
                 hasLeague = True
         return hasLeague
+
 
 # class lolasFavChamp(Macro):
 #     def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
@@ -88,8 +98,8 @@ class MacroEsportsOrLeague(Macro):
 
 class UserInfo(Macro):
     def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
-        #variables used in conversation so far.
-        #TODO We don't need to intialize the variables here, but it might be good to keep track of which variables we use
+        # variables used in conversation so far.
+        # TODO We don't need to intialize the variables here, but it might be good to keep track of which variables we use
         visits = 'VISITS'
         player = 'FAV_PLAYER'
         playerRec = 'PLAYER_RECOMMEND'
@@ -99,16 +109,17 @@ class UserInfo(Macro):
         vars[playerRec] = ''
         vars[visits] = 1
 
+
 class favRegion(Macro):
     def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
-        #opens json
+        # opens json
         f = open('resources/tourneys.json', )
         data = json.load(f)
 
-        #gets user input
+        # gets user input
         mystr = ngrams.text().split()
 
-        #different ways a user can reference the regions
+        # different ways a user can reference the regions
         regions = {'na': 'NORTH AMERICA',
                    'north america': 'NORTH AMERICA',
                    'north': 'NORTH AMERICA',
@@ -135,7 +146,7 @@ class favRegion(Macro):
                    'china': 'CHINA',
                    'jp': 'JAPAN',
                    'japan': 'JAPAN'
-        }
+                   }
 
         # labeled T because they're temporary variables that are not meant to be stored.
         # TODO make a macro to remove unnecessary variables
@@ -215,41 +226,42 @@ class favRegion(Macro):
 
         return True
 
+
 class UserInputChampion(Macro):
     def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
 
-        #variables
+        # variables
         playerRec = 'PLAYER_RECOMMEND'
         fav_champ = 'FAV_CHAMP'
 
-        #opening jason
+        # opening jason
         f = open('resources/champs.json', )
         data = json.load(f)
-        #takes user input as string
+        # takes user input as string
         mystr = ngrams.text()
 
-        #loads spacy model to tokenize string
+        # loads spacy model to tokenize string
         nlp = spacy.load("en_core_web_md")
         doc = nlp(mystr)
 
-        #turns user input into a list of strings
+        # turns user input into a list of strings
         sentenceSplit = mystr.split()
 
-        #top, mid, jungle, bot, support
+        # top, mid, jungle, bot, support
         for role in data['ontology']['lane']:
-            #iterates through each champion
+            # iterates through each champion
             for champion in data['ontology'][role]:
-                #for each token in user's sentence
+                # for each token in user's sentence
                 for token in doc:
-                    #champions are labeled as nouns by spacy
+                    # champions are labeled as nouns by spacy
                     if token.pos_ == 'NOUN':
-                        #if champion in ontology and player suggested champion is same as champion in iteration
+                        # if champion in ontology and player suggested champion is same as champion in iteration
                         if token.text in data['ontology'] and champion == token.text:
-                            #keeps track of first player in ont who plays that champion in lcs
-                            #TODO keep track of recommended players
-                            #TODO handle cases where user does not have favorite champ
-                            #TODO handle cases where user misspells champion
-                            #TODO handle cases where user has multiple favorite champions
+                            # keeps track of first player in ont who plays that champion in lcs
+                            # TODO keep track of recommended players
+                            # TODO handle cases where user does not have favorite champ
+                            # TODO handle cases where user misspells champion
+                            # TODO handle cases where user has multiple favorite champions
                             vars[fav_champ] = champion
                             vars[playerRec] = data['ontology'][token.text][0]
                             break
@@ -261,11 +273,70 @@ class UserInputChampion(Macro):
             break
         return True
 
+
 class MacroRandNum(Macro):
     def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
-        randomNum = random.randint(0,1)
-        if randomNum==1:
+        randomNum = random.randint(0, 1)
+        if randomNum == 1:
             print('yes')
             return True
         else:
             return False
+
+
+class MacroGPTJSON(Macro):
+    def __init__(self, request: str, full_ex: Dict[str, Any], empty_ex: Dict[str, Any] = None,
+                 set_variables: Callable[[Dict[str, Any], Dict[str, Any]], None] = None) -> object:
+        """
+        :rtype: object
+        :param request: the task to be requested regarding the user input (e.g., How does the speaker want to be called?).
+        :param full_ex: the example output where all values are filled (e.g., {"call_names": ["Mike", "Michael"]}).
+        :param empty_ex: the example output where all collections are empty (e.g., {"call_names": []}).
+        :param set_variables: it is a function that takes the STDM variable dictionary and the JSON output dictionary and sets necessary variables.
+        """
+        self.request = request
+        self.full_ex = json.dumps(full_ex)
+        self.empty_ex = '' if empty_ex is None else json.dumps(empty_ex)
+        self.check = re.compile(regexutils.generate(full_ex))
+        self.set_variables = set_variables
+
+    def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
+        examples = f'{self.full_ex} or {self.empty_ex} if unavailable' if self.empty_ex else self.full_ex
+        prompt = f'{self.request} Respond in the JSON schema such as {examples}: {ngrams.raw_text().strip()}'
+        output = gpt_completion(prompt)
+        if not output: return False
+
+        try:
+            print(output)
+            d = json.loads(output)
+        except JSONDecodeError:
+            print(f'Invalid: {output}')
+            return False
+
+        if self.set_variables:
+            self.set_variables(vars, d)
+        else:
+            vars.update(d)
+
+        return True
+
+    class MacroNLG(Macro):
+        def __init__(self, generate: Callable[[Dict[str, Any]], str]):
+            self.generate = generate
+
+        def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[Any]):
+            return self.generate(vars)
+
+
+def gpt_completion(input: str, regex: Pattern = None) -> str:
+    response = openai.ChatCompletion.create(
+        model=CHATGPT_MODEL,
+        messages=[{'role': 'user', 'content': input}]
+    )
+    output = response['choices'][0]['message']['content'].strip()
+
+    if regex is not None:
+        m = regex.search(output)
+        output = m.group().strip() if m else None
+
+    return output
