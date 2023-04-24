71fbd1180a0b5ba6969dcab0271e569c18da1c1d
Shong0626 <shon244@emory.edu>
2023-04-24 14:13:38 -0400

    Adding to Advanced

--- KnowLeague/advanced.py
+++ KnowLeague/advanced.py
@@ -2,7 +2,7 @@ def advanced():
     advanced = { #user has just mentioned that they actively watch the game mentioned in casual.
         'state': 'advanced',
         '`What did you think about the game?`': { #TODO: HANDLE CASES LIKE "I LIKED IT"
-            '{good, competitive, close, fun, well}': {
+            '{good, competitive, close, fun, well, liked}': {
                 '`Who do you think performed the best?`': {
                     # Should reply with if the game was close or not
                     'state': 'perform',
@@ -22,49 +22,80 @@ def advanced():
                             '{disagree, no, not, don\'t}': {
                                 '#GATE `Darn, that\'s unfortunate. Is there another skill you think` $BEST_PLAYER `did well in?`': 'choose_skills',
                                 '#GATE `Then who do you think outperformed` $BEST_PLAYER `?`': {
+                                    'state': 'outperform',
                                     '[#ONT(teams)]': {
-                                        '`That\'s debatable... in what way?`',
-                                        '`I guess that could be true...can you explain?`',
-                                        '`No way you think that! Prove it!`'
+                                        '`That\'s debatable... in what way?`': {
+                                            'error': {
+                                                '`I see. I still think that` $BEST_PLAYER`was the most influential this game.': 'end'
+                                            }
+                                        },
+                                        '`I guess that could be true...can you explain?`': {
+                                            'error': {
+                                                '`I gotcha. I still think that` $BEST_PLAYER`was the strongest player this game.`': 'end'
+                                            }
+                                        },
+                                        '`No way you think that! Prove it!`': {
+                                            'error': {
+                                                '`For sure, but I still think that` $BEST_PLAYER`was the best performer this game.`': 'end'
+                                            }
+                                        },
+                                    },
+                                    'error': {
+                                        '`I\'m not sure if that player played in this game. Maybe you misspelled their name.`': 'outperform'
                                     }
                                 }
-
+                            },
+                            '{agree, yes, yeah, sure}': {
+                                '`Is there another player that you thought performed well this game?`': {
+                                    '[$GOOD_PLAYER=#ONT(teams)]': {
+                                        '`What made him particularly stand out?`': {
+                                            '{laning, fighting, objective control, vision control, roaming, itemization, wave management}': 'skills',
+                                            'error': {
+                                                '`I\'m not so sure about that... Maybe there\'s another skill that you think he excelled at?`': 'skills'
+                                            }
+                                        }
+                                    },
+                                    '[no, not, dont, didnt, cant, bad]': {
+                                        '`That makes sense. Who do you think performed the worst then?`': 'worst'
+                                    },
+                                    'error': {
+                                        '`I see where you\'re coming from. Is there a player you think performed poorly?`': 'worst'
+                                    }
+                                }
                             },
                             'error': {
-                                '`Can you say that again, I didn\'t catch that...`': 'agreement'
+                                '`Can you reword that, I didn\'t quite catch that...`': 'agreement'
                             }
-                        },
-                        '{agree, yes, yeah, sure}': {
-                            '`Is there another player that you thought performed well this game?`': {
-                                '[$GOOD_PLAYER=#ONT(teams)]': {
-                                    '`What made him particularly stand out?`': {
-                                        '{laning, fighting, objective control, vision control, roaming, itemization, wave management}': 'skills',
-                                        'error': {
-                                            '`I\'m not so sure about that... What\'s another skill that you think he excelled at?`': 'skills'
-                                        }
-                                    }
-                                }
-                            },
-
+                        }
                     }
-
                 }
             },
             '{boring, bad, #LEM(stomp), [not close]}': {
-                '#CLOSE2` Who do you think was the winning factor for the game? I personally think it was` #BESTPLAYER': { #Should reply with if the game was close or not
+                '#CLOSE2` Who do you think was the winning factor for the game?`': { #Should reply with if the game was close or not
                     '[$BEST_PLAYER=#ONT(teams)]': {
+                        '#IF($BEST_PLAYER = #BESTPLAYER)': {
+                            'score': 1.0,
+                            '`I thought so too!`': {
 
+                            }
+                        },
+                        'score': 0.9,
+                        '`In what way?`': {
+                            'error': {
+                                '`Ohhhh, that makes a lot of sense. I personally think that` #BESTPLAYER `was the most influential. What do you think about` #BESTPLAYER`?`': {
+                                    'error': {
+                                        '`For sure. It was nice talking to you about LoL. I hope that we can talk again soon!`': 'end'
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             },
             'error': {
                 '`I\'m not sure if I understood.`': 'advanced'
-            }
+                # TODO: I guess error state is useful there when the user give other response than "good"...
             },
-
-            'error': {
-                #TODO: I guess error state is useful there when the user give other response than "good"...
-            }
 
         },
         '#GATE `Do you think anyone showed a lot of improvement over their last game?`': {
@@ -80,6 +111,7 @@ def advanced():
         },
         '#GATE `Do you think anyone has been performing worse than usual?`': {
             'score': 0.8,
+            'state': 'worst',
             '[$UNDERPERFORMING=#ONT(teams)]': {
                 '`What have they been struggling with the most lately?`': {
                     '[$WORSE_SKILL = {laning, fighting, objective control, vision control, roaming, itemization, wave management}]': {
